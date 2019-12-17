8c6da2138c5f3b12ebfa28d1e0fd3d8e056f06d8
Jeffreey Jeens Taylor <jeffreeytaylor@hotmail.com>
2019-12-17 12:58:05 -0500

    Se crearon Pages y rutas

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// Pages
+import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+// import 'package:componentes/src/pages/home_temp.dart';
+
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      // home: HomePage()
+
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/'      : (BuildContext context) => HomePage(),
+        'alert'  : (BuildContext context) => AlertPage(),
+        'avatar' : (BuildContext context) => AvatarPage(),
+      },
+
+    );
+  }
+}
